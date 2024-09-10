bfed6118950745285d603c63b339514b04872f95
TimTham123 <cdtham00@gmail.com>
2024-09-10 16:56:54 +1000

    first sound addition
    Added
    - impact sound script, applicable to any two object collisions
    - crushing block sound

---
+++ Alchemeow Unity Project/Assets/Alchemeow/Scripts/Audio/audio_impactsound.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class audio_impactsound : MonoBehaviour
+{
+    /// <summary>
+    /// GENERAL SCRIPT FOR IMPACT SOUNDS BETWEEN ONE OBJECT AN ANOTHER USING IMPACT VELOCITY PARAMETER
+    /// </summary>
+    ///
+
+
+    // OBJECT IMPACTED AGAINST
+    [SerializeField] private string collisionObject;
+
+
+    // SOUND BEING MADE
+    [SerializeField] FMODUnity.EventReference impactEvent;
+    private FMOD.Studio.EventInstance impactInstance;
+
+    private float impactSpeed;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //
+        if (collision.gameObject.name == collisionObject)
+        {
+
+            impactInstance = FMODUnity.RuntimeManager.CreateInstance(impactEvent);
+
+            impactSpeed = collision.relativeVelocity.magnitude;
+
+            // SETS VELOCITY BETWEEN VALUE 0-1
+            Mathf.Clamp(impactSpeed, 0, 3);
+            impactSpeed = (impactSpeed / 3);
+
+            impactInstance.start();
+            impactInstance.setParameterByName("impact_velocity", impactSpeed);
+            
+        }
+        else
+        {
+
+        }
+
+
+
+
+
+    }
+}
+
+    
+
