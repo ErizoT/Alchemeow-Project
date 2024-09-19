fba6a90fb84ad9e75dd50b9f1acdfa427e63d31f
JJJangJin <maxjin3272@gmail.com>
2024-09-19 19:30:28 +1000

    UI scenes + small sound fixes
    All of Kim's UI work from her project have been added to the project, under its own folder named KimUI.
    Some sound fixes based on errors seen during playtest.

---
+++ Alchemeow Unity Project/Assets/KimUI/UI/SCRIPTS/ButtonSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource mySounds;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void HoverSound()
+    {
+        mySounds.PlayOneShot(hoverSound);
+    }
+    public void ClickSound()
+    {
+        mySounds.PlayOneShot(clickSound);
+    }
+}
---
+++ Alchemeow Unity Project/Assets/KimUI/UI/SCRIPTS/Menu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+  public GameObject optionsMenu; 
+
+  public GameObject optionsFirstButton, optionsClosedButton;
+
+  public GameObject startGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenOptions()
+
+    {
+        optionsMenu.SetActive(true);
+
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+
+        //set a new selected obj
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void CloseOptions()
+
+    {
+        optionsMenu.SetActive(false);
+
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+
+        //set a new selected obj
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
+    }
+
+   
+
+        public void GameStart()
+
+    {
+        startGame.SetActive(false);
+
+        //clear selected obj 
+        EventSystem.current.SetSelectedGameObject(null);
+
+    }
+}
