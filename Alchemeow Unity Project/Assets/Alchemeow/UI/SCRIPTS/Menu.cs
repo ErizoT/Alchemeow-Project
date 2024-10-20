29a829752bfaa4adec004a2c0c1d5565349c03ac
Troshie <kimberley8scott@gmail.com>
2024-10-20 20:12:37 +1100

    UI Fully Functional and Completed
    -Fixed Controller Inputs
    -All Sprites completed

--- Alchemeow Unity Project/Assets/Alchemeow/UI/SCRIPTS/Menu.cs
+++ Alchemeow Unity Project/Assets/Alchemeow/UI/SCRIPTS/Menu.cs
@@ -12,19 +12,6 @@ public class Menu : MonoBehaviour
 
     public GameObject startGame;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-
-    }
-
     public void OpenOptions()
 
     {
@@ -48,8 +35,6 @@ public class Menu : MonoBehaviour
         //set a new selected obj
         EventSystem.current.SetSelectedGameObject(optionsClosedButton);
     }
-
-   
 
         public void GameStart()
 
